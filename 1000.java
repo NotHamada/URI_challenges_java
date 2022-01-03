ad338d4baabe5df9a0977a1297cba175ea765320
Matheus Hamada <matheushamada@live.com>
2022-01-03 20:53:36 -0300

    Solutions

--- 1000.java
+++ 1000.java
@@ -1,11 +1,11 @@
 import java.io.IOException;
- 
+
 public class Main {
- 
+
     public static void main(String[] args) throws IOException {
- 
-        print("Hello World!");
- 
+
+        System.out.println("Hello World");
+        
     }
- 
+
 }
