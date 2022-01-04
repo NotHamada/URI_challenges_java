3b954bd59af4c9a099302a8209cdc4967f28acab
Matheus Hamada <matheushamada@live.com>
2022-01-03 21:04:18 -0300

    Solutions

--- 1000.java
+++ 1000.java
@@ -1,15 +1,11 @@
 import java.io.IOException;
-import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        /* Escrever Hello World! */
-        Scanner scanner = new Scanner(System.in);
         System.out.println("Hello World!");
 
-        scanner.close();
     }
 
 }
