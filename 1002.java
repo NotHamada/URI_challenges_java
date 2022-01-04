7474bd11ecebb7f3583c1f66583c8727b290f0f7
Matheus Hamada <matheushamada@live.com>
2022-01-03 21:23:16 -0300

    Solutions

---
+++ 1002.java
@@ -0,0 +1,20 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+ 
+        Scanner scanner = new Scanner(System.in);
+
+        double r = scanner.nextDouble();
+        double pi = 3.14159;
+        double area = pi * r * r;
+
+        System.out.printf("A=%.4f%n", area);
+
+        scanner.close();
+ 
+    }
+ 
+}
